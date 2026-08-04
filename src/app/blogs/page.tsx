a4fcbabd9b393de180e32847dbc93121c5177c25
Mujtaba Mohamed <mujtabamohamed@gmail.com>
2026-08-04 19:56:23 +0530

    test

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,10 +1,10 @@
-import { TextShimmer } from '@/components/ui/text-shimmer';
+import { TextShimmer } from "@/components/ui/text-shimmer";
 
 export default function BlogsPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <TextShimmer className='font-mono text-lg' duration={3}>
-        Comming Soon...
+      <TextShimmer className="font-mono text-lg" duration={3}>
+        Comming Soon......
       </TextShimmer>
     </div>
   );
