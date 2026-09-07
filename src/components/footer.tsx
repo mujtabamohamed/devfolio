3c0246f7fa5c07bebd447ba52d13734feb78808a
Mujtaba Mohamed <mujtabamohamed@gmail.com>
2026-09-08 05:26:19 +0530

    update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,12 +4,12 @@ export function Footer() {
       {/* <div className="mx-auto max-w-6xl px-4">
         <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
           <div className="flex items-center gap-3 justify-centert"> */}
-            {/* <div className="text-2xl font-semibold">Mujtaba Mohamed</div> */}
-          {/* </div>
+      {/* <div className="text-2xl font-semibold">Mujtaba Mohamed</div> */}
+      {/* </div>
           <div className="md:col-span-2 flex flex-col items-center justify-items-center text-sm text-muted-foreground space-y-2 text-center md:text-left">
             <p>&copy; 2025 Mujtaba Mohamed. All rights reserved.</p>
             <p>- Inspired by Chánh Đại</p> */}
-            {/* <p>
+      {/* <p>
               Inspired by <Link href="https://tailwindcss.com" className="underline underline-offset-4">tailwindcss</Link> & {""}
               <Link href="https://ui.shadcn.com" className="underline underline-offset-4">ui.shadcn.com</Link>.
             </p>
@@ -22,18 +22,13 @@ export function Footer() {
               <span className="rounded border px-2 py-0.5 text-xs">DMCA Protected</span>
               <a href="#top" className="ml-auto rounded border px-2 py-0.5 text-xs">↑ Top</a>
             </div> */}
-          {/* </div>
+      {/* </div>
         </div>
       </div> */}
       <div className="flex flex-col items-center justify-items-center text-center space-y-2 text-xs sm:text-sm px-8 sm:px-12 border-y py-4 text-muted-foreground text-wrap">
-        <p>&copy; 2025 Mujtaba Mohamed. All rights reserved.</p>
-        <p>Inspired by Chánh Đại</p>
+        <p>&copy; 2026 Mujtaba Mohamed. All rights reserved.</p>
+        {/* <p>Inspired by Chánh Đại</p> */}
       </div>
-
-
-
-
-
     </footer>
   );
 }
